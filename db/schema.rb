1acab306da503c4802e21c0853d2c7803ce25f31
Drew Daniels <daniels_drew@protonmail.com>
2023-07-14 20:04:43 -0500

    create association between users and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_15_010314) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_15_010423) do
   create_table "comments", force: :cascade do |t|
     t.string "body"
     t.datetime "created_at", null: false
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_15_010314) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_15_010314) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
